698276fde75c733a64c7b2e1c27bc0a34fc18736
gillesah <helleugilles@gmail.com>
2024-03-08 16:45:24 +0100

    change the footer

--- movie-front/src/components/FooterMood.vue
+++ movie-front/src/components/FooterMood.vue
@@ -1,7 +1,7 @@
 <template>
 	<footer class="footer">
 		<div class="footer-content d-flex justify-content-center">
-			created by <a href="https://gilleshelleu.com/" target="_blank"> Gilles Helleu</a> - using the <a href="https://www.themoviedb.org/" target="_blank">TMDB database</a>
+			created by <a href="https://gilleshelleu.com/" target="_blank">&nbsp;Gilles Helleu</a> - using the <a href="https://www.themoviedb.org/" target="_blank">&nbsp;TMDB database</a>
 		</div>
 	</footer>
 </template>
